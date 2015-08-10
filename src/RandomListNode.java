fc2271bad3c43c5106a297e10a2dd136b162cb0a
xsun89 <sunxin89@gmail.com>
2015-08-10 16:23:50 -0700

    aaa

---
+++ src/RandomListNode.java
@@ -0,0 +1,10 @@
+/**
+ * Created by sun38 on 8/10/2015.
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next, random;
+    RandomListNode(int label){
+        this.label = label;
+    }
+}
--- src/Solutions.java
+++ src/Solutions.java
@@ -1,6 +1,56 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by xsun on 15-08-09.
  */
 public class Solutions {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node){
+        if(node == null){
+            return null;
+        }
+        ArrayList<UndirectedGraphNode> nodes = new ArrayList<UndirectedGraphNode>();
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> map =
+                new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
 
+        nodes.add(node);
+        map.put(node, new UndirectedGraphNode(node.label));
+        int start = 0;
+        while(start <nodes.size()){
+            UndirectedGraphNode head = nodes.get(start++);
+            for(int i=0; i<head.neighbors.size(); i++){
+                UndirectedGraphNode neighbor = head.neighbors.get(i);
+                if(!map.containsKey(neighbor)){
+                    map.put(neighbor, new UndirectedGraphNode(neighbor.label));
+                    nodes.add(neighbor);
+                }
+            }
+        }
+
+        for (int i = 0; i < nodes.size(); i++) {
+            UndirectedGraphNode newNode = map.get(nodes.get(i));
+            for (int j = 0; j < nodes.get(i).neighbors.size(); j++) {
+                newNode.neighbors.add(map.get(nodes.get(i).neighbors.get(j)));
+            }
+
+        }
+
+        return map.get(node);
+    }
+
+    public RandomListNode copyRandomList(RandomListNode head){
+        if(head == null){
+            return null;
+        }
+        ArrayList<RandomListNode> list = new ArrayList<RandomListNode>();
+        HashMap<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+        RandomListNode tmp = head;
+        while(tmp !=null){
+            list.add(tmp);
+            map.put(tmp, new RandomListNode(tmp.label));
+            tmp = tmp.next;
+        }
+
+
+    }
 }
